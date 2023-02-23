cc65cf9bba7b4f7a94446f29f24811f2e1fff59c
Stamen Stoychev <SStoychev@infragistics.com>
2023-02-23 17:14:11 +0200

    allowing 'run test' to run without an env platform defined

--- test/transformation.spec.ts
+++ test/transformation.spec.ts
@@ -6,11 +6,14 @@ describe("Transformation Tests", () => {
     const testPlatform = process.env.npm_config_testPlatform || "";
 
     it("should not have any files with leftover replacement words", async () => {
-        const result = await SearchHandler.searchInFiles(
-            [SearchHandler.buildFullRegex(testPlatform)],
-            [testPlatform as Platform],
-            allLangs,
-            defaultFileGroups.allMarkdown);
-        expect(result).to.eql([]);
+        let testPlatforms = !testPlatform.length ? allPlatforms : [testPlatform];
+        for (const platform of testPlatforms) {
+            const result = await SearchHandler.searchInFiles(
+                [SearchHandler.buildFullRegex(platform)],
+                [platform as Platform],
+                allLangs,
+                defaultFileGroups.allMarkdown);
+            expect(result).to.eql([]);
+        }
     });
 });
